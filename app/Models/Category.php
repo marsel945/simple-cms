9236c57d6de3122157a617e2b7b1d054d9797b8f
marsel945 <rewoherson519@gmail.com>
2023-10-11 12:37:33 +0800

    add: categories model,controller & migration

---
+++ app/Models/Category.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'parent_id',
+        'name',
+        'slug',
+        'status'
+    ];
+
+    public function parent()
+    {
+        return $this->belongsTo('App\Category', 'parent_id');
+    }
+    public function children()
+    {
+        return $this->hasMany('App\Category', 'parent_id');
+    }
+}
