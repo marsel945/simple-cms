31c86a20450f0b3fbf5d41d3a37d8bc567d5bc21
marsel945 <rewoherson519@gmail.com>
2023-10-11 13:09:19 +0800

    Add: createCategoryRequest

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateCategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -27,9 +28,10 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateCategoryRequest $request)
     {
-        //
+        $data = $request->all();
+        dd($data);
     }
 
     /**
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,7 +11,7 @@ class Category extends Model
 
     protected $fillable = [
         'parent_id',
-        'name',
+        'title',
         'slug',
         'status'
     ];
