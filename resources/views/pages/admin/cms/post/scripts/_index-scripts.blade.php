6aa283a734c1e8a9e26bd8f12c53a26c4e41b69a
marsel945 <rewoherson519@gmail.com>
2023-10-11 22:08:24 +0800

    Add: script post datatable

---
+++ resources/views/pages/admin/cms/post/scripts/_index-scripts.blade.php
@@ -0,0 +1,79 @@
+<script type="text/javascript">
+    var table = $("#dataTablePosts");
+    $(document).ready(function() {
+        table.DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: {
+                url: "{{ route('admin.cms.posts.datatable') }}",
+                'type': "POST",
+                'data': (d) => {
+                    d._token = '{{ csrf_token() }}'
+                }
+            },
+            columns: [{
+                    name: "check",
+                    data: "check",
+                    orderable: false,
+                    searchable: false,
+                    render: function(d) {
+                        return d == null ? "Tidak Ada" : d;
+                    },
+                },
+                {
+                    name: "title",
+                    data: "title",
+                    render: function(d) {
+                        return d == null ? "Tidak Ada" : d;
+                    },
+                },
+                {
+                    name: "category",
+                    data: "category",
+                    orderable: true,
+                    searchable: true,
+                    render: function(d) {
+                        return d == null ? "Tidak Ada" : d;
+                    },
+                },
+                {
+                    name: "date",
+                    data: "date",
+                    orderable: true,
+                    searchable: true,
+                    render: function(d) {
+                        return d == null ? "Tidak Ada" : d;
+                    },
+                },
+                {
+                    name: "author",
+                    data: "author",
+                    orderable: true,
+                    searchable: true,
+                    render: function(d) {
+                        return d == null ? "Tidak Ada" : d;
+                    },
+
+                },
+
+                {
+                    name: "status",
+                    data: "status",
+                    orderable: true,
+                    searchable: true,
+                    render: function(d) {
+                        return d == null ? "Tidak Ada" : d;
+                    },
+                },
+                {
+                    name: "action",
+                    data: "action",
+                    render: function(d) {
+                        return d == null ? "Tidak Ada" : d;
+                    },
+                },
+            ],
+            columnDefs: [],
+        });
+    });
+</script>
