badd594e65ae17092a7996010990fdc65a7e9d4f
marsel945 <rewoherson519@gmail.com>
2023-10-11 22:04:01 +0800

    Add: datatabe rendering function

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Helpers\FormatDate;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class PostController extends Controller
 {
@@ -17,11 +19,78 @@ class PostController extends Controller
     }
 
     /**
+     * 
+     * Render Datatable post resource
+     * 
+     */
+
+    public function datatable()
+    {
+        $query = Post::query()->orderBy('updated_at');
+
+        return DataTables::of($query)
+            ->addColumn('check', function ($item) {
+                $element = '<div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="categoryCheck1">
+                                <label class="form-check-label" for="categoryCheck1"></label>
+                            </div>';
+                return $element;
+            })
+            ->addColumn('title', fn ($item) => $item->title)
+            ->addColumn('category', fn ($item) => Post::find($item->category_id)->category->title)
+            ->addColumn('date', fn ($item) => FormatDate::getDateTimeCutMonth($item->created_at))
+            ->addColumn('author', function ($item) {
+                $author = Post::find($item->user_id)->author->name;
+                $element = '';
+                $element .= '<div class="d-flex align-items-center">';
+                $element .= '<img src="../../assets/images/avatar/avatar-7.jpg"';
+                $element .= 'alt="" class="rounded-circle avatar-xs me-2" />';
+                $element .= '<h5 class="mb-0">' . $author  . '</h5>';
+                $element .= '</div>';
+
+                return $element;
+            })
+            ->addColumn('status', function ($item) {
+                $element = '';
+
+                if ($item->status == 'draft') {
+                    $element = '<span class="badge-dot bg-warning me-1 d-inline-block align-middle"></span>Draft';
+                } else if ($item->status == 'published') {
+                    $element = '<span class="badge-dot bg-success me-1 d-inline-block align-middle"></span>Published';
+                } else if ($item->status = 'scheduled') {
+                    $element = '<span class="badge-dot bg-info me-1 d-inline-block align-middle"></span>Scheduled';
+                }
+                return $element;
+            })
+            ->addColumn('action', function ($item) {
+                $element = '';
+                $element .= '<span class="dropdown dropstart">';
+                $element .= '<a class="btn-icon btn btn-ghost btn-sm rounded-circle" href="#" role="button" id="courseDropdown1" data-bs-toggle="dropdown" data-bs-offset="-20,20" aria-expanded="false">';
+                $element .= '<i class="fe fe-more-vertical"></i>';
+                $element .= '</a>';
+                $element .= '<span class="dropdown-menu" aria-labelledby="courseDropdown1">';
+                $element .= '<span class="dropdown-header">Settings</span>';
+                $element .= '<a class="dropdown-item" href="#"><i class="fe fe-edit dropdown-item-icon"></i>Edit</a>';
+                $element .= '<a class="dropdown-item" href="#"><i class="fe fe-move dropdown-item-icon"></i>Move</a>';
+                $element .= '<a class="dropdown-item" href="#"><i class="fe fe-copy dropdown-item-icon"></i>Copy</a>';
+                $element .= '<a class="dropdown-item" href="#"><i class="fe fe-toggle-left dropdown-item-icon"></i>Publish</a>';
+                $element .= '<a class="dropdown-item" href="#"><i class="fe fe-toggle-right dropdown-item-icon"></i>Unpublish</a>';
+                $element .= '<a class="dropdown-item" href="#"><i class="fe fe-trash dropdown-item-icon"></i>Delete</a>';
+                $element .= '</span></span>';
+
+                return $element;
+            })
+            ->rawColumns(['check', 'author', 'status', 'action'])
+            ->make(true);
+    }
+
+
+    /**
      * Show the form for creating a new resource.
      */
     public function create()
     {
-        //
+        return view('pages.admin.cms.post.add');
     }
 
     /**
