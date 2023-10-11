9d7cb32d7ad2bff4d9f0cfaefa74b0e75adb5f4d
marsel945 <rewoherson519@gmail.com>
2023-10-11 12:40:14 +0800

    Add: admin view template

---
+++ resources/views/pages/admin/index.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <script>
+        // Render blocking JS:
+        if (localStorage.theme) document.documentElement.setAttribute("data-theme", localStorage.theme);
+    </script>
+
+    <!-- Favicon icon-->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/images/favicon/favicon.ico') }}">
+
+
+    <!-- Libs CSS -->
+    <link href="{{ asset('assets/fonts/feather/feather.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/libs/bootstrap-icons/font/bootstrap-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/libs/%40mdi/font/css/materialdesignicons.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/libs/simplebar/dist/simplebar.min.css') }}" rel="stylesheet">
+
+
+
+
+    <!-- Theme CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/theme.min.css') }}">
+    <link rel="canonical" href="layout-horizontal.html">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- Wrapper -->
+
+    <div id="db-wrapper">
+        @include('layouts.partials.sidebar')
+
+        <main id="page-content">
+            @include('layouts.partials.admin_navbar')
+            @yield('content')
+        </main>
+    </div>
+    <!-- Wrapper -->
+
+    @yield('modal')
+
+
+    <!-- Scripts -->
+    <!-- Libs JS -->
+
+    <script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/simplebar/dist/simplebar.min.js') }}"></script>
+
+
+    <!-- Theme JS -->
+    <script src="{{ asset('assets/js/theme.min.js') }}"></script>
+
+    <script src="{{ asset('assets/libs/flatpickr/dist/flatpickr.min.js') }}"></script>
+    <script src="{{ asset('assets/js/vendors/flatpickr.js') }}"></script>
+
+    <script src="{{ asset('assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('assets/js/vendors/chart.js') }}"></script>
+
+
+    @stack('customJsQuery')
+</body>
+
+</html>
