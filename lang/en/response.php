6b8ffbdcf3990d7f88e6d6f2e23b86596c5ec120
marsel945 <rewoherson519@gmail.com>
2023-10-13 05:00:20 +0800

    Add: language response to alert

---
+++ lang/en/response.php
@@ -0,0 +1,15 @@
+<?php
+
+
+return [
+    'success' => [
+        'store' => ':data was successfully added',
+        'update' => ':data was successfully updated',
+        'delete' => ':data was successfully deleted',
+    ],
+    'error' => [
+        'store' => 'failed to add :data',
+        'update' => 'failed to update :data',
+        'delete' => 'failed to delete :data',
+    ],
+];
