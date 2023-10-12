25750766841dc17eb17a13e752e5806e05b0b56c
marsel945 <rewoherson519@gmail.com>
2023-10-12 20:59:13 +0800

    Fix: rules

--- app/Http/Requests/CreatePostRequest.php
+++ app/Http/Requests/CreatePostRequest.php
@@ -15,15 +15,15 @@ class CreatePostRequest extends FormRequest
     }
 
     /**
-     * Get the validation rules that apply to the request.
+     * Get the validation rules that apply to the request. 
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
         return [
-            'title' => 'string|min:8|max:255',
-            'excerpt' => 'string|min:8|max:255',
+            'title' => 'string|max:255',
+            'excerpt' => 'string|max:255',
         ];
     }
 }
