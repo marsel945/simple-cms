8be194a6e9d298a32d04cb5340e03dc0600f414f
marsel945 <rewoherson519@gmail.com>
2023-10-12 20:56:34 +0800

    Add: filepond

--- resources/views/pages/admin/cms/post/add.blade.php
+++ resources/views/pages/admin/cms/post/add.blade.php
@@ -2,7 +2,10 @@
 
 @push('customCss')
     <link rel="stylesheet" href="{{ asset('assets/libs/bootstrap-select/dist/css/bootstrap-select.min.css') }}" />
-    <link href="{{ asset('assets/libs/dropzone/dist/dropzone.css') }}" rel="stylesheet">
+
+    <!-- FilePond Css -->
+    <link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css" rel="stylesheet" />
+    <link href="https://unpkg.com/filepond@^4/dist/filepond.css" rel="stylesheet" />
 @endpush
 
 @section('content')
@@ -19,7 +22,7 @@
                                 <li class="breadcrumb-item">
                                     <a href="admin-dashboard.html">Dashboard</a>
                                 </li>
-                                <li class="breadcrumb-item"><a href="#">CMS</a></li>
+                                <li class="breadcrumb-item"><a href="{{ route('admin.cms.overview') }}">CMS</a></li>
                                 <li class="breadcrumb-item active" aria-current="page">
                                     Add New Post
                                 </li>
@@ -57,46 +60,40 @@
                             <i class="fe fe-link me-1"></i>
                             Link
                         </button>
-                        <form action="#" class="dropzone mt-4 border-dashed rounded-2 min-h-0"
+                        <form action="{{ route('admin.cms.post.upload.attachment') }}" id="formUpload"
                             enctype="multipart/form-data">
+                            @csrf
                             <div class="fallback">
-                                <input name="file" type="file" multiple>
+                                {{-- <input name="image" type="file" multiple> --}}
                             </div>
                         </form>
                         <div class="mt-4">
                             <form action="{{ route('admin.cms.posts.store') }}" method="POST">
                                 @csrf
+                                <div class="mb-3 col-md-12">
+                                    <input name="image" type="file" multiple>
+                                </div>
                                 <!-- Form -->
                                 <div class="row">
                                     <div class="mb-3 col-md-12">
                                         <!-- Title -->
                                         <label for="postTitle" class="form-label">Title</label>
-                                        <input type="text" id="postTitle" name="postTitle" class="form-control text-dark"
+                                        <input type="text" id="postTitle" name="title" class="form-control text-dark"
                                             placeholder="Post Title">
                                         <small>Keep your post titles under 60 characters. Write
                                             heading that describe the topic content.
                                             Contextualize for Your Audience.</small>
                                     </div>
-                                    <!-- Slug -->
-                                    <div class="mb-3 col-md-12">
-                                        <label for="basic-url" class="form-label">Slug</label>
-                                        <div class="input-group mb-1">
-                                            <span class="input-group-text" id="basic-addon3">https://example.com/</span>
-                                            <input type="text" class="form-control" id="basic-url" name="postSlug"
-                                                aria-describedby="basic-addon3">
-                                        </div>
-                                        <small>Field must contain an unique value</small>
-                                    </div>
                                     <!-- Excerpt -->
                                     <div class="mb-3 col-md-12">
                                         <label for="Excerpt" class="form-label">Excerpt</label>
-                                        <textarea rows="3" id="Excerpt" class="form-control text-dark" placeholder="Excerpt" name="postExcerpt"></textarea>
+                                        <textarea rows="3" id="Excerpt" class="form-control text-dark" placeholder="Excerpt" name="excerpt"></textarea>
                                         <small>A short extract from writing.</small>
                                     </div>
                                     <!-- Category -->
                                     <div class="mb-3 col-md-6">
                                         <label class="form-label">Category</label>
-                                        <select class="selectpicker" data-width="100%" name="category">
+                                        <select class="selectpicker" data-width="100%" name="category_id">
                                             <option value="">--Select Category--</option>
                                             @foreach ($data['categories'] as $category)
                                                 <option value="{{ $category->id }}">{{ $category->title }}</option>
@@ -107,10 +104,10 @@
                                 <!-- Editor -->
                                 <div class="mt-2 mb-4">
                                     <textarea id="editor" name="content">
-                                        <h4>One Ring to Rule Them All</h4>
+                                        {{-- <h4>One Ring to Rule Them All</h4> --}}
                                     </textarea>
                                 </div>
-                                <button type="submit" class="btn btn-primary"> Publish </button>
+                                <button type="submit" class="btn btn-primary">Publish</button>
                                 <!-- button -->
                                 <a href="#" class="btn btn-outline-secondary">
                                     Save to Draft
@@ -216,15 +213,32 @@
 @endsection
 
 @push('customJsQuery')
-    <script src="{{ asset('assets/libs/dropzone/dist/min/dropzone.min.js') }}"></script>
     <script src="{{ asset('assets/libs/bootstrap-select/dist/js/bootstrap-select.min.js') }}"></script>
     <script src="{{ asset('assets/libs/quill/dist/quill.min.js') }}"></script>
-
     <!--CKEDITOR -->
-
     <script src="//cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
 
+    <!-- FilePond Js -->
+    <script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js"></script>
+    <script src="https://unpkg.com/filepond@^4/dist/filepond.js"></script>
+
+
     <script>
+        FilePond.registerPlugin(FilePondPluginImagePreview);
+        // Get a reference to the file input element
+        const inputElement = document.querySelector('input[type="file"]');
+
+        // Create a FilePond instance
+        const pond = FilePond.create(inputElement);
+        FilePond.setOptions({
+            server: {
+                process: "{{ route('admin.cms.post.upload.attachment') }}",
+                revert: "{{ route('admin.cms.post.delete.attachment') }}",
+                headers: {
+                    'X-CSRF-TOKEN': "{{ csrf_token() }}"
+                }
+            },
+        });
         $(document).ready(function() {
 
             CKEDITOR.replace('editor');
